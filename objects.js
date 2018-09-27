ceb0a78fc9e6692f4e43b615c200972dcf6acecd
joneseyit <joneseyit@protonmail.com>
2018-09-27 00:23:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,9 @@
 var meals = {};
 var meals = new Object();
 var meals = {breakfast: "oatmeal"};
+
+const firstMeal = 'breakfast'
+var meals = {firstMeal: 'oatmeal'}
+var meals = {[firstMeal]: 'oatmeal'}
+
+meals.breakfast
