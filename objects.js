e2548fd023d61914900913bb1d1f92836fd9802c
joneseyit <joneseyit@protonmail.com>
2018-09-27 00:41:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,6 @@ var playlist = {mark: "Onipa"}
 
 function updatePlaylist(obj, artiste, title){
   // updatedList = Object.assign( {}, playlist, {[artiste]: [title] })
+  playlist.artiste = title;
+  return playlist
 }
