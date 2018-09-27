fbdcc397264fb16558071f4713eb8d3eaf77060a
joneseyit <joneseyit@protonmail.com>
2018-09-27 00:35:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,1 @@
-var meals = {};
-var meals = new Object();
-var meals = {breakfast: "oatmeal"};
-
-const firstMeal = 'breakfast'
-var meals = {firstMeal: 'oatmeal'}
-var meals = {[firstMeal]: 'oatmeal'}
-
-meals.breakfast
-meals['breakfast']
-meals.firstMeal = undefind
-
-Object.assign()
+var playlist = {
